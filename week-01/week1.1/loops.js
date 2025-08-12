c68ceedc81e07d09b762a064c5610df2062c5795
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2025-08-12 16:50:30 +0500

    loops

---
+++ week-01/week1.1/loops.js
@@ -0,0 +1,30 @@
+// Loops:
+
+/* Write a JavaScript program to print all natural numbers from 1 to n using a while loop.
+
+Take the value of n as input from the user.
+
+Print each number on a new line.
+
+*/
+
+let n = 10;  // in node we can't take prompt
+
+let i = 0;
+
+while(i <= n){
+    console.log(i)
+    i++
+}
+
+/* Write a JavaScript program to print all natural numbers in reverse (from n to 1) using a while loop.
+
+Take the value of n as input from the user.
+
+Print each number on a new line in reverse order.
+
+*/
+while(n >= i) {
+    console.log(n)
+    n--;
+}
